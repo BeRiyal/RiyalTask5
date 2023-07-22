0ce3c1e230d0b27b03dcee39195aae7479d7c394
riyal patel <rbp.fetr@gmail>
2023-07-21 21:28:26 -0400

    remove error

--- app/src/main/java/com/example/riyaltask5/MainActivity.java
+++ app/src/main/java/com/example/riyaltask5/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 import com.example.riyaltask5.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMainBinding mainBinding;
     @Override
@@ -19,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(view);
         init();
+    }
+
+    public void onClick(View  v){
+        if(v.getId()==mainBinding.btnSubmit.getId()){
+            if(mainBinding.etName.getText().toString().trim().isEmpty()){
+                mainBinding.etName.setError("Can't be empty");
+            }
+            else {
+                mainBinding.etName.setError(null);
+            }
+            if(mainBinding.etEmail.getText().toString().trim().isEmpty()){
+                mainBinding.etEmail.setError("Can't be empty");
+            }
+            else {
+                mainBinding.etName.setError(null);
+            }
+            if(mainBinding.etPhone.getText().toString().trim().isEmpty()){
+                mainBinding.etPhone.setError("Can't be empty");
+            }
+            else {
+                mainBinding.etName.setError(null);
+            }
+        }
     }
     private void init(){
         mainBinding.btnSubmit.setOnClickListener(this);

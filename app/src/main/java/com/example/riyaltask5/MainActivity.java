1aecc7b9c576f718e1c9ab4bb763c9c0119d5214
riyal patel <rbp.fetr@gmail>
2023-07-22 11:49:48 -0400

    validation and added details activity_details.xml

--- app/src/main/java/com/example/riyaltask5/MainActivity.java
+++ app/src/main/java/com/example/riyaltask5/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.riyaltask5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,7 @@ import com.example.riyaltask5.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,25 +25,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick(View  v){
         if(v.getId()==mainBinding.btnSubmit.getId()){
-            if(mainBinding.etName.getText().toString().trim().isEmpty()){
-                mainBinding.etName.setError("Can't be empty");
-            }
-            else {
-                mainBinding.etName.setError(null);
-            }
-            if(mainBinding.etEmail.getText().toString().trim().isEmpty()){
-                mainBinding.etEmail.setError("Can't be empty");
-            }
-            else {
-                mainBinding.etName.setError(null);
-            }
-            if(mainBinding.etPhone.getText().toString().trim().isEmpty()){
-                mainBinding.etPhone.setError("Can't be empty");
-            }
-            else {
-                mainBinding.etName.setError(null);
+            if(isValid()){
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+                SharedPreferences.Editor prefEdit = sharedPreferences.edit();
+
+                prefEdit.putString("name", mainBinding.etName.getText().toString().trim());
+                prefEdit.putString("email", mainBinding.etEmail.getText().toString().trim());
+                prefEdit.putString("pass", mainBinding.etPhone.getText().toString().trim());
+
+                prefEdit.commit();
+
             }
         }
+    }
+
+    private boolean isValid() {
+        if(mainBinding.etName.getText().toString().trim().isEmpty()){
+            mainBinding.etName.setError("Can't be empty");
+            return false;
+        }
+        else {
+            mainBinding.etName.setError(null);
+        }
+        if(mainBinding.etEmail.getText().toString().trim().isEmpty()){
+            mainBinding.etEmail.setError("Can't be empty");
+            return false;
+        }
+        else {
+            mainBinding.etName.setError(null);
+        }
+        if(mainBinding.etPhone.getText().toString().trim().isEmpty()){
+            mainBinding.etPhone.setError("Can't be empty");
+            return false;
+        }
+        else {
+            mainBinding.etName.setError(null);
+        }
+        return true;
     }
     private void init(){
         mainBinding.btnSubmit.setOnClickListener(this);
